52d570cbd9c6a1a19dca4adc8abd79913e77d837
Anton Shlykov <permanent.as@gmail.com>
2019-03-04 18:35:43 +0200

    Added routing

--- src/component/FinishedQuiz/FinishedQuiz.js
+++ src/component/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './FinishedQuiz.css'
 import Button from '../UI/Button/Button';
+import { Link } from 'react-router-dom';
 
 const FinishedQuiz = (props) => {
     console.log(props.results);
@@ -44,12 +45,10 @@ const FinishedQuiz = (props) => {
                 >
                 Повторить
             </Button>
-            <Button 
-                onClick={props.onRetry} 
-                type="success"
-                >
-                Перейти в список тестов
-            </Button>
+            <Link to='/'>
+                <Button type='success'>Перейти в список тестов</Button>
+            </Link>
+            
         </div>
     )
 }
